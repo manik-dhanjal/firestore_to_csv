8dd7a149c0906ec769366002bcd55f4f6bf92da3
manik-dhanjal <manikdhanjal217@gmail.com>
2022-09-04 20:13:10 +0530

    index route added

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const odomosRoutes = require("./src/routes/odomos.routes");
 
 app.use('/odomos-quiz', odomosRoutes);
 
+app.get('/',(req,res) =>{
+    res.send(`
+        <HTML>
+            <Head>
+                <Title> 
+                 
+                </Title>
+            </Head>
+            <Body>
+                <a href="/odomos-quiz">click here for Odomos CSV file</a>
+            </Body>
+        </HTML>
+    `)
+})
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT,() => {
